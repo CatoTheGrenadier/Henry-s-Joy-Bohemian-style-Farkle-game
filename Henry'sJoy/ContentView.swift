7a34c110ddd4f03193f0dbf698e341697020a7fc
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2024-02-10 22:43:13 -0500

    Initial Commit

---
+++ Henry'sJoy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Henry'sJoy
+//
+//  Created by Yi Ling on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
